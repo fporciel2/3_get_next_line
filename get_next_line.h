1e69d3da6b3c645b60b5918f24b6fc868954ba9e
fporciel <fedeporcus@gmail.com>
2023-03-29 16:38:04 +0200

    get_next_line for 42

---
+++ get_next_line.h
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fporciel <fporciel@student.42roma.it>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/26 18:24:52 by fporciel          #+#    #+#             */
+/*   Updated: 2023/03/28 08:57:19 by fporciel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/* 
+* This software is made available to anyone who wants to retrace the 
+* author's learning path through the projects of school 42.
+* Copyright (C) 2023  Federico Porciello
+* 
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*
+* You can contact the author at: 
+*- fedeporcus@protonmail.com
+*- Via Piantarose 7A 06121 Perugia (PG) Italia (Italy) 
+*/
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stddef.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+char	*get_next_line(int fd);
+
+#endif
